172a45367692bbe64eb61b7184c96311b65d55e8
当然海霏丝 <2232629199@qq.com>
2023-07-02 17:42:54 +0800

    V8.2.3.3

--- app/src/main/java/com/example/code06/NewsAdapter.java
+++ app/src/main/java/com/example/code06/NewsAdapter.java
@@ -8,55 +8,63 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.code06.News;
 import com.example.code06.R;
 
 import java.util.List;
 
- public class NewsAdapter extends ArrayAdapter <News> {
+ public class NewsAdapter extends RecyclerView.Adapter <NewsAdapter.ViewHolder > {
 
      private List<News> mNewsData;
      private Context mContext;
      private int resourceId;
 
      public NewsAdapter(Context context, int resourceId, List<News> data) {
-         super(context, resourceId, data);
+
          this.mContext = context;
          this.mNewsData = data;
          this.resourceId = resourceId;
      }
 
      @Override
-     public View getView(int position, View convertView, ViewGroup parent) {
-         News news = getItem(position);
-         View view;
+     public ViewHolder onCreateViewHolder(ViewGroup parent ,
+                                                                       int viewType) {
+         View view = LayoutInflater.from(mContext)
+         .inflate(resourceId , parent , false);
+         ViewHolder holder = new ViewHolder(view);
+         return holder;
+         }
 
-         ViewHolder viewHolder;
+     @Override
+     public void onBindViewHolder(ViewHolder holder ,
+                                                               int position) {
+          News news = mNewsData.get(position);
+          holder.tvTitle.setText(news.getTitle());
+          holder.tvAuthor.setText(news.getAuthor());
 
-         if (convertView == null) {
-          view = LayoutInflater.from(getContext())
-          .inflate(resourceId , parent , false);
+          if (news.getImageId() != -1) {
+         holder.ivImage.setImageResource(news.getImageId());
+         }
+          }
 
-          viewHolder = new ViewHolder();
-             viewHolder.tvTitle = view.findViewById(R.id.tv_title);
-            viewHolder.tvAuthor = view.findViewById(R.id.tv_subtitle);
-            viewHolder.ivImage = view.findViewById(R.id.iv_image);
+     @Override
+     public int getItemCount() {
+          return mNewsData.size();
+          }
 
-        view.setTag(viewHolder);
-        } else {
-        view = convertView;
-        viewHolder = (ViewHolder) view.getTag();
-        }
+     static class ViewHolder extends RecyclerView.ViewHolder {
+          TextView tvTitle;
+          TextView tvAuthor;
+          ImageView ivImage;
 
-          viewHolder.tvTitle.setText(news.getTitle());
-          viewHolder.tvAuthor.setText(news.getAuthor());
-          viewHolder.ivImage.setImageResource(news.getImageId());
+          public ViewHolder(View view) {
+             super(view);
 
-          return view;
-     }
-     class ViewHolder {
-         TextView tvTitle;
-         TextView tvAuthor;
-         ImageView ivImage;
+             tvTitle = view.findViewById(R.id.tv_title);
+             tvAuthor = view.findViewById(R.id.tv_subtitle);
+             ivImage = view.findViewById(R.id.iv_image);
+             }
          }
  }
